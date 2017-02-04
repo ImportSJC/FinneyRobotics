bad475637727d21fabd104fade92a31f56273373
robotics <robotics@CFS368.office.local>
2017-02-04 13:11:48 -0500

    Creating the basic AutoMode class. It needs to be implemented still.

--- src/cpi/AutoMode.java
+++ src/cpi/AutoMode.java
@@ -1,5 +1,23 @@
 package cpi;
 
-public class NewClass {
+import cpi.auto.SuperClass;
 
+public class AutoMode {
+	private SuperClass[][] matrix;
+	private String         ID;
+	private String         desc;
+	public AutoMode(SuperClass[][] matrix, String ID, String desc) {
+		this.matrix = matrix;
+		this.ID     = ID;
+		this.desc   = desc;
+	}
+	public String getID() {
+		return this.ID;
+	}
+	public String getDesc() {
+		return this.desc;
+	}
+	public SuperClass[][] getMatrix() {
+		return this.matrix;
+	}
 }
